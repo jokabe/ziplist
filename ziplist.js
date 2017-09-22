3313d1d969f9c24c05c7ec8cc79af6266659c90f
jokabe <okabej3@gmail.com>
2017-09-22 00:08:45 -1000

    fixed spacing

--- ziplist.js
+++ ziplist.js
@@ -2,7 +2,7 @@ const first = ['a', 'b', 'c'];
 const next = [1, 2, 3];
 
 function zipList(one, two) {
-  const newArray = [];
+  const newArray =[ ];
   for (let i = 0; i < one.length; i++) {
     newArray.push(one[i]);
     newArray.push(two[i]);
